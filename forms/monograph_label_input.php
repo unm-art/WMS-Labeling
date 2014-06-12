300e7ad0758ad4ba0980a0e54677f103e5417ceb
Cameron Stewart <bacchus@unm.edu>
2014-06-12 15:36:59 -0600

    Barcode input-view functional.  Not ready for processing.

---
+++ forms/monograph_label_input.php
@@ -0,0 +1,34 @@
+
+<script>
+    addBarcodeInput();
+</script>
+<form name="barcode_scan_form" action="lib/monograph_label_controller.php">
+    <div class="barcodes_forms_container">
+
+        <h1 class="form_header">Enter Barcode Number</h1>
+
+        <div class="output_columns_radio_container">
+            <p>Would you like to print to <b>one</b> or <b>two</b> column label stock?</p>
+            <input type="radio" name="output_columns" value="1" checked="checked">One
+            <input type="radio" name="output_columns" value="2">Two
+        </div>
+
+        <div id="barcodes_container">
+
+            <table id="barcodes_table" class="barcodes_table">
+                <tr>
+                    <th>Barcode No.</th><th>Pocket Label?</th>
+                </tr>
+
+                <!-- generated rows of barcode inputs go here -->
+
+            </table>
+
+        </div>
+
+        <div class="button_container">
+            <input class="btn_add input_center" type="button" value="+1" onClick="addBarcodeInput()">
+            <input class="btn_submit input_center" type="submit" value="Make Labels">
+        </div>
+    </div>
+</form>
