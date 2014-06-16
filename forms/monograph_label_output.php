60646b6b02f5632c387105f3836920b61ffdee70
Brian Freels-Stendel <bfreels@unm.edu>
2014-06-16 13:14:57 -0600

    Update locations, css; fix print position start; rectify variable settings between monograph_label_output and quicklabels.

--- forms/monograph_label_output.php
+++ forms/monograph_label_output.php
@@ -2,6 +2,7 @@
 <script src="../jquery/jquery-ui-1.10.4/js/jquery-ui-1.10.4.custom.min.js"></script>
 <script src="../jquery/printarea/jquery.PrintArea.js" type="text/JavaScript" language="javascript"></script>
 <script src="../scripts/js_functions.js" language="JavaScript"></script>
+<link rel="stylesheet" href="../css/wms.css">
 <?php
 
 //var_dump($_REQUEST);
@@ -35,13 +36,12 @@ else {
     // Error
 }
 
+if (isset($_REQUEST["label_start"]) && is_numeric($_REQUEST["label_start"]) && $_REQUEST["label_start"] < count($num_array) && $_REQUEST["label_start"] > 0 && $_REQUEST["label_start"] < 9) {
+    $label_start = $_REQUEST["label_start"];
+}
+
 $barcode_count = count($barcodes);
 
-if (isset($_REQUEST["label_start"]) && is_numeric($_REQUEST["label_start"]) && $_REQUEST["label_start"] < count($num_array) && $_REQUEST["label_start"] > 0 && $_REQUEST["label_start"] < 9) {
-    $print_position = $_REQUEST["label_start"];
-} else {
-    $print_position = "0";
-}
 
 $print_array = array();
 
@@ -51,50 +51,44 @@ include_once('../lib/quicklabels.php');
 
 for($x = 0; $x < $barcode_count; $x++) {
     $print_array[] = quicklabels($barcodes[$x], $print_pocket_label[$x]);
-    //var_dump($print_array[$x]);
+//    var_dump($print_array[$x]);
+}
+
+// Add empty rows to print_array to start print on specified label
+if (isset($label_start) && $label_start != "") {
+    for ($x = 1; $x < $label_start; $x++) {
+        array_unshift($print_array, array("&nbsp;", "&nbsp;"));
+    }
 }
 
 $table_row = "";
+$table = "";
 
-for ($x = 0; $x < $print_position; $x++) {
-    $table_row .= "<tr>\n";
-    $table_row .= "<td class=\"cnum\">&nbsp;</td>\n<td class=\"pocket\">&nbsp;</td>\n";
-    if (isset($output_columns) && $output_columns == "2" && array_key_exists($x + 1, $print_array)) {
-        $x++;
-        $table_row .= "<td class=\"spacer_cell\">&nbsp;</td>\n";
-        $table_row .= "<td class=\"cnum\">&nbsp;</td>\n<td class=\"pocket\">&nbsp;</td>\n";
-    }
-    if (isset($output_columns) && $output_columns == "2" && $x % 2 != 0) {
-        $table_row .= "</tr>\n";
-        $table_row .= "<tr>\n<td class=\"spacer_cell_vert\" colspan=\"5\">&nbsp;</td>\n</tr>\n";
-    }
-
-}
-
-
-for ($x = $print_position; $x < count($print_array); $x++) {
-    print_r("1");
-    var_dump($output_columns);
-    if (isset($output_columns) && $output_columns == "2" && $x % 2 != 0) {
-        print_r("2");
+for ($x = 0; $x < count($print_array); $x++) {
+//    print_r("1");
+//    var_dump($output_columns);
         $table_row .= "<tr>\n";
         $table_row .= "<td class=\"cnum\">{$print_array[$x][0]}</td>\n<td class=\"pocket\">{$print_array[$x][1]}</td>\n";
-    }
     if (isset($output_columns) && $output_columns == "2" && array_key_exists($x + 1, $print_array)) {
-        print_r("3");
+ //       print_r("3");
         $x++;
         $table_row .= "<td class=\"spacer_cell\">&nbsp;</td>\n";
         $table_row .= "<td class=\"cnum\">{$print_array[$x][0]}</td>\n<td class=\"pocket\">{$print_array[$x][1]}</td>\n";
     }
     $table_row .= "</tr>\n";
     $table_row .= "<tr>\n<td class=\"spacer_cell_vert\" colspan=\"5\">&nbsp;</td>\n</tr>\n";
+    // Test whether we have eight labels for 2-col printing, or are on the last label
+    if ((($x + 1) % 8 == 0) || $x + 1 == count($print_array)) {
+        $table .= "<table class=\"label_table\">\n{$table_row}</table>\n";
+        $table_row = "";
+    }
 }
 
-$table = "<table class=\"label_table\">\n{$table_row}</table>\n";
+
 
 ?>
 
-<div id="link-area" class="printhidden">
+<div id="link-area" class="print_label_button">
     <div id="link-print"><a id="print_button" href="#print"><img src="../images/icon-print.png" /><br/>Print Labels</a></div>
 </div>
 
